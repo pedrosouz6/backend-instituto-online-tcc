ecc27712febacba24f28e97cb2ea3611b24b802a
Pedro Souza <peedrosouz6@gmail.com>
2022-09-12 08:55:10 -0300

    adding database port

--- src/services/connection/index.ts
+++ src/services/connection/index.ts
@@ -1,9 +1,10 @@
 import { createConnection } from 'mysql2';
 import 'dotenv/config';
 
-export const connect = createConnection({
+export const connect = createConnection({  
     database: process.env.DATABASE,
     host: process.env.HOST,
     password: process.env.PASSWORD,
-    user: process.env.USER
+    user: process.env.USER,
+    port: Number(process.env.PORT),
 });
