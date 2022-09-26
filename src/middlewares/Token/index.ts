f4c17ad3789268b153f216985c85ab8f38719be6
Pedro Souza <peedrosouz6@gmail.com>
2022-09-26 11:51:47 -0300

    returns user data

--- src/middlewares/Token/index.ts
+++ src/middlewares/Token/index.ts
@@ -1,15 +1,22 @@
 import { NextFunction, Request, Response } from "express";
-import { verify } from "jsonwebtoken";
+import { JwtPayload, verify } from "jsonwebtoken";
+import { connect } from "../../services/connection";
 import { config } from "../../services/jwt";
 
 export class MiddlewareToken {
-    validateToken(req: Request, res: Response, next: NextFunction): Response {
+    async validateToken(req: Request, res: Response, next: NextFunction): Promise<Response> {
         const { token } = req.body;
 
         try {
-            verify(token, config.secret);
+            const { id } = verify(token, config.secret) as JwtPayload;
+            
+            const sql = 'SELECT * FROM users WHERE id = ?';
+            const values = [ id ];
+
+            const [ results ] = await connect.promise().query(sql, values);
 
             return res.status(200).json({
+                results,
                 error: false,
                 message: 'O token está válido'
             })
