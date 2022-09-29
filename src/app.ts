a802df676df0d2bcde8bb3f400ff76b3a8f28a98
Pedro Souza <peedrosouz6@gmail.com>
2022-09-29 10:49:25 -0300

    cleaning ap.js

--- src/app.ts
+++ src/app.ts
@@ -15,33 +15,4 @@ app.use(express.urlencoded({extended: true}));
 app.use('/api', userRoutes);
 app.use('/api', tokenRoute);
 
-app.get('/test/:limit/:pageNumber', async (req, res): Promise<Response> => {
-        const { limit, pageNumber } = req.params;
-        
-        const start = ( Number(limit) * Number(pageNumber) ) - Number(limit);
-        const sql = `SELECT * FROM users ORDER BY id ASC LIMIT ${start}, ${limit}`;
-        const sqlPagination = "SELECT id FROM users";
-
-        try {
-            const [ resultsPagination ]: [RowDataPacket[], FieldPacket[]] = await connect.promise().query(sqlPagination);
-            const [ results ] = await connect.promise().query(sql);
-
-            const totalUsers = resultsPagination.length;
-            const totalPages = Math.ceil(totalUsers / Number(limit));
-
-            return res.status(200).json({
-                error: false,
-                totalUsers,
-                totalPages,
-                results
-            });
-            
-        } catch(err) {
-            return res.status(406).json({
-                error: true,
-                message: "Erro ao pegar os usuário do banco de dados"
-            });
-        }
-})
-
 export { app };
