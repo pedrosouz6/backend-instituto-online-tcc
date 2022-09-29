9a5d84ee427fe942481508dafbe9ead48b29acb5
Pedro Souza <peedrosouz6@gmail.com>
2022-09-29 07:50:38 -0300

    test heroku

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
-import express from 'express';
+import express, { Response } from 'express';
 import cors from 'cors';
 
 import { userRoutes } from './routes/User';
 import { tokenRoute } from './routes/Token';
+import { FieldPacket, RowDataPacket } from 'mysql2';
+import { connect } from './services/connection';
 
 const app = express();
 
@@ -11,5 +13,34 @@ app.use(express.json());
 
 app.use('/api', userRoutes);
 app.use('/api', tokenRoute);
+
+app.get('/test/:limit/:pageNumber', async (req, res): Promise<Response> => {
+        const { limit, pageNumber } = req.params;
+        
+        const start = ( Number(limit) * Number(pageNumber) ) - Number(limit);
+        const sql = `SELECT * FROM users ORDER BY id ASC LIMIT ${start}, ${limit}`;
+        const sqlPagination = "SELECT id FROM users";
+
+        try {
+            const [ resultsPagination ]: [RowDataPacket[], FieldPacket[]] = await connect.promise().query(sqlPagination);
+            const [ results ] = await connect.promise().query(sql);
+
+            const totalUsers = resultsPagination.length;
+            const totalPages = Math.ceil(totalUsers / Number(limit));
+
+            return res.status(200).json({
+                error: false,
+                totalUsers,
+                totalPages,
+                results
+            });
+            
+        } catch(err) {
+            return res.status(406).json({
+                error: true,
+                message: "Erro ao pegar os usuário do banco de dados"
+            });
+        }
+})
 
 export { app };
